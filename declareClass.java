7375d304647ddaa5fb84fe52f71c673b8bb26f46
Deepak Yadav <alfo7742@gmail.com>
2021-05-20 13:25:13 +0530

    sequence of calling b/w classes

--- declareClass.java
+++ declareClass.java
@@ -1,7 +1,24 @@
 class a
 {
-public static void main (String args[])
-{
-	System.out.println("hello world");
+	public static void main (String args[])
+	{
+		System.out.println("hello world");
+		B b = new B();
+       		System.out.println(b.i); //prints 40
+	}
 }
+
+class B
+{ 
+	int i;  //instance variable 
+	B()
+	{	
+		 System.out.println(i);  //10
+		 i=40;			//update of i value  i.e: i=40 
+	}
+	 
+	{ //first this block copied before the contructor is called	
+		 System.out.println(i);  //i=0
+		 i=10; 			//update in i value i.e.: i=10 
+        }
 }
