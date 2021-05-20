cadefa77350d5e23c6c6ed1a8ed6523c5848ad29
Deepak Yadav <alfo7742@gmail.com>
2021-05-20 13:24:34 +0530

    declare variable

---
+++ datatype.java
@@ -0,0 +1,19 @@
+class datatype
+{
+	public static void main(String args[])
+	{
+		String name = "John";
+		System.out.println("hello"+name);
+		
+		int myNum = 15;   //can also declare 1st and assign value later
+		System.out.println(myNum);
+		//use "final" if dont want to change the value
+		
+
+		int myNum = 5;	 //4byte
+		float myFloatNum = 5.99f; 	//4byte
+		char myLetter = 'D'; 	//2byte
+		boolean myBool = true; 	//1bit
+		String myText = "Hello";  		
+	}
+}
