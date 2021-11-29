7d5b471d31a40acd7a30a693aa32e56c7847255b
Deepak Yadav <alfo7742@gmail.com>
2021-11-29 19:27:25 +0530

    adding basic java

---
+++ helloworld.java
@@ -0,0 +1,15 @@
+class a {
+    public static void main(String[] args) {
+        System.out.println("Hello WOrld !!! Deepak");
+
+        for(int i=0;i<4;i++)
+        {
+            if(i==2)
+            {
+            System.out.println(i++);
+            break;
+            }
+        }
+        
+    }
+}
---
+++ visitwebsite.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class visitwebsite {
+    public static void main(String[] args) 
+    {
+        HttpURLConnection http;
+        try {
+         URL url = new URL("http://152.70.199.233");
+            http = (HttpURLConnection)url.openConnection();
+            System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
+            http.disconnect();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+             
+    }
+}
