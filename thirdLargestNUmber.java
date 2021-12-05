ba70f26df70eff0e96ef1d348150aff24163fc07
Deepak Yadav <alfo7742@gmail.com>
2021-12-05 20:34:14 +0530

    day 2

--- thirdLargestNUmber.java
+++ thirdLargestNUmber.java
@@ -1,4 +1,5 @@
-/*Third largest element 
+/* Day 2 
+Third largest element 
 
 Given an array of distinct elements. Find the third largest element in it.
 Suppose you have A[] = {1, 2, 3, 4, 5, 6, 7}, its output will be 5 because it is the 3 largest element in the array A.
