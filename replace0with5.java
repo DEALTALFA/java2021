5b92132597797c499736d59d20e1fef1a1ce4a24
Deepak Yadav <alfo7742@gmail.com>
2021-12-05 20:33:56 +0530

    day 1

--- replace0with5.java
+++ replace0with5.java
@@ -1,22 +1,24 @@
-// You are given an integer N. You need to convert all zeroes of N to 5.
-// Example 1:
-// Input:
-// N = 1004
-// Output: 1554
-// Explanation: There are two zeroes in 1004
-// on replacing all zeroes with "5", the new
-// the number will be "1554".
+/*  Day 1
+You are given an integer N. You need to convert all zeroes of N to 5.
+Example 1:
+Input:
+N = 1004
+Output: 1554
+Explanation: There are two zeroes in 1004
+on replacing all zeroes with "5", the new
+the number will be "1554".
  
-// Example 2:
-// Input:
-// N = 121
-// Output: 121
-// Explanation: Since there are no zeroes in
-// "121", the number remains as "121".
-// Expected Time Complexity: O(K) where K is the number of digits in N
-// Expected Auxiliary Space: O(1)
-// Constraints:
-// 1 <= n <= 10000
+Example 2:
+Input:
+N = 121
+Output: 121
+Explanation: Since there are no zeroes in
+"121", the number remains as "121".
+Expected Time Complexity: O(K) where K is the number of digits in N
+Expected Auxiliary Space: O(1)
+Constraints:
+1 <= n <= 10000
+*/
 import java.util.Scanner;
 public class replace0with5 {
     public static void main(String[] args) {
