3f01799d136e0a0824e5bb13743d9fb02f605199
Deepak Yadav <alfo7742@gmail.com>
2021-12-05 20:29:39 +0530

    day 3

--- binaryarraysorting.java
+++ binaryarraysorting.java
@@ -1,4 +1,5 @@
-/*Given a binary array A[] of size N. The task is to arrange the array in increasing order.
+/* DAY 3
+Given a binary array A[] of size N. The task is to arrange the array in increasing order.
 Note: The binary array contains only 0  and 1.
 Example 1:
 Input:
